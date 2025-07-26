17e34d411d40970a4a8ff3fdb824bcad3cc95a9c
jadsoncerqueira <jadsoncerqueira97@gmail.com>
2025-07-25 21:35:06 -0300

    Dominio Curso e mentoria

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Aprenda os conceitos básicos de Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de JavaScript");
+        curso2.setDescricao("Aprenda os conceitos básicos de JavaScript");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Desenvolvimento de Software"); 
+        mentoria.setDescricao("Sessão de mentoria sobre desenvolvimento de software");
+        mentoria.setData(java.time.LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
